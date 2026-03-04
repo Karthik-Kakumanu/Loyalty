fec35657b7891834250bd4488625ab4eb321c661
Karthik Kakumanu <22jr1a0497@gmail.com>
2026-03-04 21:11:19 +0530

    Req

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,12 @@ export const metadata: Metadata = {
   description: "Your favorite cafes in one app.",
   manifest: "/manifest.json",
   icons: {
-    icon: "/icon/icon-192x193.png",
-    apple: "/icon/icon-192x193.png",
+    icon: [
+      { url: "/icon/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    shortcut: "/icon/icon-192x192.png",
+    apple: [{ url: "/icon/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   appleWebApp: {
     capable: true,
